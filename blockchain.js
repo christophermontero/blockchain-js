dc6611ec7c6aef6ef80ccec53d3e932951b7e237
christophermontero <cgortizm21@gmail.com>
2021-12-21 16:55:56 -0500

    Method to create a new block is added

--- blockchain.js
+++ blockchain.js
@@ -2,3 +2,19 @@ function Blockchain() {
   this.chain = [];
   this.newTransations = [];
 }
+
+Blockchain.prototype.createNewBlock = function (nonce, prevBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransations,
+    nonce,
+    hash,
+    prevBlockHash
+  };
+
+  this.newTransations = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
