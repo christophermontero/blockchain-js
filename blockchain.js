601fd199e6ccd477dc9dd059302b68bcdc63d931
christophermontero <cgortizm@gmail.com>
2021-12-22 14:18:54 -0500

    Hashing block method is added

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require("sha256");
+
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
@@ -37,6 +39,19 @@ Blockchain.prototype.createNewTransaction = function (
   this.pendingTransactions.push(newTransaction);
 
   return this.getLastBlock()["index"] + 1;
+};
+
+Blockchain.prototype.hashBlock = function (
+  prevBlockHash,
+  currBlockData,
+  nonce
+) {
+  const dataAsString =
+    prevBlockHash + nonce.toString() + JSON.stringify(currBlockData);
+
+  const hash = sha256(dataAsString);
+
+  return hash;
 };
 
 module.exports = Blockchain;
