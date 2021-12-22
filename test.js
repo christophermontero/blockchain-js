f4ef440653b3d8ee767d72e84773f8bdcba96410
christophermontero <cgortizm@gmail.com>
2021-12-22 13:13:40 -0500

    Test create new transaction method

--- test.js
+++ test.js
@@ -2,16 +2,16 @@ const Blockchain = require("./blockchain");
 
 const testCoin = new Blockchain();
 
-testCoin.createNewBlock(2056, "jfklsajfklajfljkla", "jfklejaklfjeajefjalfjea");
-testCoin.createNewBlock(
-  11,
-  "jjksajdklfklsajfklajfljkla",
-  "jlfjkldajkfklejaklfjeajefjalfjea"
-);
-testCoin.createNewBlock(
-  20,
-  "jkjdflajfdjlafklsajfklajfljkla",
-  "jfklejaklfjeajeajfkladjdljjalfjea"
-);
+testCoin.createNewBlock(2056, "previousHash1", "currentHash1");
 
-console.log(testCoin);
+testCoin.createNewTransaction(100, "senderAddress", "recipientAddress");
+
+testCoin.createNewBlock(29320483, "previousHash2", "currentHash2");
+
+testCoin.createNewTransaction(50, "senderAddress", "recipientAddress");
+testCoin.createNewTransaction(300, "senderAddress", "recipientAddress");
+testCoin.createNewTransaction(500, "senderAddress", "recipientAddress");
+
+testCoin.createNewBlock(2932, "previousHash3", "currentHash3");
+
+console.log(testCoin.chain[2]);
