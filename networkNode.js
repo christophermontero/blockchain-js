a2b9ebe6b2ce3bfa76928fd978fb2c612b099cfe
christophermontero <cgortizm@gmail.com>
2022-01-08 09:21:26 -0500

    Create separate instances for the network

--- networkNode.js
+++ networkNode.js
@@ -5,11 +5,11 @@ require("colors");
 require("./src/startup/config")(app);
 require("./src/startup/endpoints")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.argv[2];
 
 app.listen(port, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${port}`.yellow
+    `Node is running in ${process.env.NODE_ENV} mode on port ${port}`.yellow
       .bold
   );
 });
