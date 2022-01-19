1d033c9d4fd9fde3bfa3b095cd6a6d4bafb44b9d
christophermontero <cgortizm@gmail.com>
2022-01-19 10:27:56 -0500

    Method getAddressData is created

--- blockchain.js
+++ blockchain.js
@@ -140,4 +140,27 @@ Blockchain.prototype.getTransaction = function (transId) {
   };
 };
 
+Blockchain.prototype.getAddressData = function (address) {
+  const addressTransaction = [];
+
+  this.chain.forEach((block) => {
+    block.transactions.forEach((transaction) => {
+      if (transaction.sender === address || transaction.recipient === address) {
+        addressTransaction.push(transaction);
+      }
+    });
+  });
+
+  let balance = 0;
+
+  addressTransaction.forEach((transaction) => {
+    if (transaction.recipient === address) balance += transactin.amount;
+    else if (transaction.sender === address) balance -= transaction.amount;
+  });
+
+  return {
+    addressTransaction,
+    balance
+  };
+};
 module.exports = Blockchain;
